305522af863993ba0c1240a5f549a996e22536c4
allabban <abdallahallabban12@gmail.com>
2024-04-18 09:12:08 +0300

    added a cv page

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-if (isset($_GET['ename'])){
+if (isset($_GET['G231210563@gmail.com']) && isset($_GET[''])){
     echo "Welcome :". $_GET["ename"]."<br>";
 }else{
     header("location: https://allabban.github.io/login.html")
