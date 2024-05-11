f251481d1cfc218f29e6d83ae7bede085cd1988e
allabban <165964632+allabban@users.noreply.github.com>
2024-05-12 02:32:26 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,1 @@
-<?php
-if (isset($_GET['G231210563@gmail.com']) && isset($_GET['G231210563'])){
-    echo "Welcome :". $_GET["ename"]."<br>";
-}else{
-    header("location: https://allabban.github.io/login.html");
-}
-?>
+
